433f5953b81ada43b9cdd32540c2650bf8d8d28c
lindakai2016 <398017990@qq.com>
2017-06-05 23:45:44 +0800

    s

--- js/page.js
+++ js/page.js
@@ -3,9 +3,15 @@ $(function(){
     //lazyload
     $("img.lazy").lazyload({effect: "fadeIn"});
     //加载评论信息
+    moreComment(0);
+});
+
+//加载更多评论信息
+function moreComment(offset){
     $.ajax({
-        url:"",
-        data:{mid:mid,chapter:chapter,offset:0},
+        url:"/php/loadComment.php",
+        data:{mid:mid,chapter:chapter,offset:offset},
+        dataType:"json",
         success:function(data){
             for(var i=0;i<data.length;i++){
                 var li=template("tpl-li",{
@@ -16,26 +22,18 @@ $(function(){
                 $("#cm").append(li);
                 offset++;
             }
-        }
-    });
-});
-
-//加载更多评论信息
-function moreComment(){
-    $.ajax({
-        url:"",
-        data:{mid:mid,chapter:chapter,offset:offset},
-        success:function(data){
-            for(var i=0;i<data.length;i++){
-                var li=template("tpl-li",{
-                    user:data[i].user,
-                    date:data[i].date,
-                    text:data[i].text
+            //limit 默认10
+            if(data.length<10){
+                $("#cm-load").unbind();
+                $("#cm-load").html(function(){
+                    return $(this).html().replace("加载更多","已加载全部");
                 });
-                $("#cm").append(li);
-                offset++;
             }
         }
     });
 }
+
+$("#cm-load").click(function(){
+    moreComment($("#cm").find(".ci").length);
+});
 
--- php/loadComment.php
+++ php/loadComment.php
@@ -1,18 +1,22 @@
 <?php
 require("config.php");
 
-$mid = isset($_GET["mid"]) or die("404");
-$chapter = isset($_GET["chapter"]) or die("404");
-$offset = isset($_GET["offset"]) or die("404");
+$mid=$_GET["mid"];
+$chapter =$_GET["chapter"];
+$offset=$_GET["offset"];
 
 $conn = new mysqli($mysql["host"], $mysql["user"], $mysql["password"], $mysql["database"]);
-if ($conn->connect_error) {
+if($conn->connect_error){
     die("数据库连接错误");
 }
-$sql = "select * from comment";
-$result = $conn->query($sql);
-if()
-while($row = $result->fetch_assoc()){
-    //$row["id"]
+$conn->set_charset("utf8");
+$stmt = $conn->prepare("select * from comment where mid=? and chapter=? limit 10 offset ?");
+if($stmt){
+    $stmt->bind_param("iii",$mid,$chapter,$offset);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+    $result->close();
+    $stmt->close();
 }
 $conn->close();
