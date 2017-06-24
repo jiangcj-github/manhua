49c52e899d060d86f1d63193c04d65de69fc7982
lindakai2016 <398017990@qq.com>
2017-06-24 10:24:47 +0800

    s

--- manhua/php/config.php
+++ manhua/php/config.php
@@ -2,7 +2,8 @@
 //error_reporting(0);
 
 $mysql=array(
-    "host"=>"192.241.217.195",
+    //"host"=>"192.241.217.195",
+    "host"=>"localhost",
     "user"=>"root",
     "password"=>"",
     "database"=>"manhua",
