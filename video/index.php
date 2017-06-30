6375e76003ce88c72dd714144fa7698d713da8d9
lindakai2016 <398017990@qq.com>
2017-06-30 10:45:31 +0800

    s

---
+++ video/index.php
@@ -0,0 +1,34 @@
+<?php require_once("../php/resource.php") ?>
+<html>
+    <head>
+        <title>視頻</title>
+        <meta charset="utf-8">
+        <meta name="keywords" content="xxx,xxx,xxx">
+        <meta name="description" content="xxxxx">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./default/css/page.css" rel="stylesheet">
+    </head>
+    <body style="background: black;">
+        <div class="row">
+            <div class="col">
+                <div class="vpre">
+                    <img src="1.png">
+                </div>
+
+            </div>
+            <div class="col">
+
+            </div>
+            <div class="col">2</div>
+            <div class="col">3</div>
+            <div class="col">3</div>
+        </div>
+        <div class="row">
+            <div class="col">3</div>
+            <div class="col">3</div>
+            <div class="col">3</div>
+            <div class="col">3</div>
+            <div class="col">3</div>
+        </div>
+    </body>
+</html>
