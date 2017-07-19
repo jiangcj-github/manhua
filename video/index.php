df6c989335e2c2df9c50f0148d0a38b7e5808513
lindakai2016 <398017990@qq.com>
2017-07-19 22:53:15 +0800

    s

--- video/index.php
+++ video/index.php
@@ -348,12 +348,12 @@
             </div>
 
             <div class="page-ctrl">
-                <a href="#">上一页</a>
+                <a href="#">上一頁</a>
                 <a href="#">1</a>
                 <a href="#">2</a>
                 <a href="#">3</a>
                 <a href="#">4</a>
-                <a href="#">下一页</a>
+                <a href="#">下一頁</a>
             </div>
         </div>
 
