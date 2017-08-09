f000a4873ebc36cfc91a3658766241efe7dca29d
lindakai2016 <398017990@qq.com>
2017-08-09 12:18:40 +0800

    s

--- video/upload.php
+++ video/upload.php
@@ -1,6 +1,7 @@
 <?php
 //獲取資源服务器token
-$ip=$_SERVER["REMOTE_ADDR"];
+//$ip=$_SERVER["REMOTE_ADDR"];
+$ip="127.0.0.1";
 $secret="lindakai";
 $time=(new DateTime())->getTimestamp();
 $_token=md5($ip.$time.$secret);
@@ -21,7 +22,7 @@ $_token=md5($ip.$time.$secret);
         .progress{
             background:#f90;
             height: 6px;
-            width:50%;
+            width:0;
             border-radius:2px;
         }
     </style>
@@ -40,7 +41,11 @@ $_token=md5($ip.$time.$secret);
             <div class="progress-wrap">
                 <div class="progress"></div>
             </div>
-            <input id="vInput" type="file" name="vInput" accept="video/mp4">
+            <div class="progress-text"></div>
+            <button class="addBtn">添加...</button>
+            <button class="startBtn" disabled="disabled">上傳</button>
+            <button class="cancelBtn" disabled="disabled">取消</button>
+            <input id="vInput" type="file" accept="video/mp4" style="display:none;">
         </div>
         <input type="text" placeholder="title">
         <select>
@@ -55,125 +60,10 @@ $_token=md5($ip.$time.$secret);
 <script src="/common/fileupload/jquery.iframe-transport.js"></script>
 <script src="/common/fileupload/jquery.fileupload.js"></script>
 <script src="/common/common.js"></script>
-
+<script src="web/js/upload.js"></script>
 <script>
-    /*
-    var upload={};
-    upload.vShow=$(".vShow");
-    upload.vInput=$("#vInput");
-    upload.vProgress="";
-    upload.init=function(){
-      var _this=this;
-      _this.vInput.change(function(){_this.upload();});
-      _this.vShow.click(function(){_this.showFileChooser();});
-    };
-    upload.showFileChooser=function(){
-        this.vInput.click();
-    };
-    upload.upload=function(){
-        var _this=this;
-        /*
-        var formData = new FormData();
-        formData.append("file", $(this)[0].files[0]);
-        $.ajax({
-            url: "/admin/video/update/uploadPoster",
-            type: "POST",
-            data: formData,
-            contentType:false,
-            processData:false,
-            xhr:function(){
-                myXhr = $.ajaxSettings.xhr();
-                if(myXhr.upload){
-                    myXhr.upload.addEventListener("progress",function(e){
-                        if (e.lengthComputable) {
-                            var per = e.loaded/e.total*100;
-                            $(".poster .progress-bar").css("width",per+"%");
-                        }
-                    },false);
-                }
-                return myXhr;
-            },
-            success: function(data){
-                if(data.msg){
-                    $("#upload_error").show();
-                    $("#upload_error").children("span").text("文件"+$("#poster")[0].files[0].name+"上传失败，"+data.msg);
-                }else{
-                    $(".poster img").prop("src",data.url);
-                }
-            }
-        });
-
-        var formData = new FormData();
-        formData.append("file", $(_this.vInput)[0].files[0]);
-        $(_this.vInput).fileupload({
-            type:"post",
-            url:"upload/upload.php",
-            dataType: "json",
-            data: formData,
-            contentType:false,
-            processData:false,
-            add: function (e, data) {
-                console.log(data);
-                data.submit();
-            },
-            progressall: function(e, data) {
-                var progress = parseInt(data.loaded / data.total * 100, 10);
-                console.log(progress);
-            },
-            done: function (e, data) {
-                console.log(e);
-                console.log(data);
-            }
-        });
-
-    };*/
-
-
-    $(function(){
-       //upload.init();
-    });
-
     var _token="<?php echo $_token; ?>";
     var _time=<?php echo $time; ?>;
-    var task;
-    $('#vInput').fileupload({
-        maxChunkSize: 2000000, // 5 MB
-        dataType: "json",
-        url:"http://lindakai.com/upload/index.php?_token="+_token+"&_time="+_time,
-        add: function (e, data) {
-            task=data.submit();
-            task.success && task.success(function(file){
-                var name=file.vInput[0].name;
-                ajaxForm.action(null,{
-                    type:"post",
-                    url:"http://lindakai.com/upload/deal.php",
-                    data:{_token:_token,_time:_time,name:name},
-                    success:function(data){
-                        if(data.ok){
-                            $(".vShow").prop("src",data.data.png);
-                        }
-                    }
-                });
-            });
-        }
-    });
-
-
-    function deal(){
-        ajaxForm.action(null,{
-            type:"post",
-            url:"http://lindakai.com/upload/deal.php",
-            data:{_token:"222",name:"test.mp4"},
-            success:function(data){
-                if(data.ok){
-                    console.log(data.data.url);
-                }
-            }
-        });
-    }
-
-    //task.abort();
-
 </script>
 </body>
 </html>
---
+++ video/web/js/upload.js
@@ -0,0 +1,157 @@
+var upload={};
+upload.vShow=$(".vShow");
+upload.vInput=$("#vInput");
+upload.addBtn=$(".addBtn");
+upload.startBtn=$(".startBtn");
+upload.cancelBtn=$(".cancelBtn");
+upload.vProgress=$(".progress");
+upload.vProgressText=$(".progress-text");
+upload.task=null;
+upload.upSpeed={st:0,sb:0};
+upload.onChange=function(){
+    this.startBtn.prop("disabled",false);
+    this.cancelBtn.prop("disabled",false);
+};
+upload.init=function(){
+    var _this=this;
+    _this.initUpload();
+    _this.vInput.attr("onchange","upload.onChange()");
+    _this.addBtn.click(function(){_this.vInput.click();});
+    _this.cancelBtn.click(function(){_this.task && _this.task.abort();});
+};
+upload.initUpload=function(){
+    var _this=this;
+    _this.vInput.fileupload({
+        maxChunkSize: 2000000, // 2MB
+        dataType: "json",
+        paramName:"vInput",
+        singleFileUploads:false,
+        url:"http://lindakai.com/upload/index.php?_token="+_token+"&_time="+_time,
+        add: function (e, data) {
+            var file=data.files[0];
+            if(!/^[0-9a-zA-Z_-]+\.mp4$/.test(file.name)){
+                log("文件名不符合規則，不能包含除0-9a-zA-Z_-之外的字符");
+                return;
+            }
+            if(file.type!="video/mp4"){
+                log("文件類型不被接受，僅支持MP4格式");
+                return;
+            }
+            if(file.size>200*1024*1024||file.size<1*1024*1024){
+                log("文件太大或者太小，必須在[1M,200M]之間");
+                return;
+            }
+            _this.showFile(file);
+            var that = this;
+            _this.startBtn.click(function(){
+                $.getJSON("http://lindakai.com/upload/index.php?_token="+_token+"&_time="+_time,{vInput:data.files[0].name},function(rs){
+                    data.uploadedBytes = rs.vInput && rs.vInput.size;
+                    if(data.uploadedBytes==data.files[0].size){
+                        var name=data.files[0].name;
+                        _this.startBtn.prop("disabled",true);
+                        _this.cancelBtn.prop("disabled",true);
+                        _this.deal(name);
+                    }else{
+                        _this.task=data.submit();
+                        _this.upSpeed.st=new Date().getTime();
+                        _this.upSpeed.sb=data.uploadedBytes;
+                        _this.task.success && _this.task.success(function(file){
+                            var name=file.vInput[0].name;
+                            _this.startBtn.prop("disabled",true);
+                            _this.cancelBtn.prop("disabled",true);
+                            _this.deal(name);
+                        });
+                    }
+                });
+            });
+        },
+        progressall:function(e,data) {
+            var now=new Date().getTime();
+            var gapt=(now-_this.upSpeed.st)/1000;
+            var gapb=data.loaded-_this.upSpeed.sb;
+            var speed=gapb/gapt;
+            var spare=(data.total-data.loaded)/speed;
+            _this.upSpeed.st=now;
+            _this.upSpeed.sb=data.loaded;
+            var speed_str=_this.formatSpeed(speed);
+            var spare_str=_this.formatSpare(spare);
+
+            //
+            console.log(speed_str);
+            console.log(spare_str);
+            //
+            var progress = Math.round(data.loaded/data.total*100);
+            _this.vProgress.css("width",progress+"%");
+            _this.vProgressText.text(progress+"%");
+        }
+    });
+};
+upload.formatSpeed=function(speed){
+    // byte/s
+    if(speed>=1024*1024){
+        return Math.round(speed/(1024*1024)) + " M/s";
+    }else if(speed>1024){
+        return Math.round(speed/1024) + " K/s";
+    }else{
+        return Math.round(speed) + " B/s"
+    }
+};
+upload.formatSpare=function(spare){
+    //s
+    var h,m,s;
+    if(spare>60*60){
+        h = Math.floor(spare/(60*60));
+        spare-=h*60*60;
+        m = Math.floor(spare/60);
+        spare-=m*60;
+        s = Math.floor(spare);
+        if(h<10){
+            h="0"+h;
+        }
+        if(m<10){
+            m="0"+m;
+        }
+        if(s<10){
+            s="0"+s;
+        }
+        return h+":"+m+":"+s;
+    }else if(spare>60){
+        m = Math.floor(spare/60);
+        spare-=m*60;
+        s = Math.floor(spare);
+        if(m<10){
+            m="0"+m;
+        }
+        if(s<10){
+            s="0"+s;
+        }
+        return m+":"+s;
+    }else{
+        s = Math.floor(spare);
+        if(s<10){
+            s="0"+s;
+        }
+        return s;
+    }
+};
+upload.deal=function(name){
+    var _this=this;
+    ajaxForm.action(null,{
+        type:"post",
+        url:"http://lindakai.com/upload/deal.php",
+        data:{_token:_token,_time:_time,name:name},
+        success:function(data){
+            if(data.ok){
+                _this.vShow.prop("src",data.data.png);
+                console.log(data.data);
+            }
+        }
+    });
+};
+upload.showFile=function(file){
+    console.log(file);
+};
+
+$(function(){
+    upload.init();
+});
