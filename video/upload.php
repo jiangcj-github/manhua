4370fab0cb5e5105dbc9d3346e4ba6cdc5562855
lindakai2016 <398017990@qq.com>
2017-08-19 17:28:09 +0800

    Merge branch 'master' of https://github.com/lindakai2016/manhua
    # Conflicts:
    #	video/upload.php

--- video/upload.php
+++ video/upload.php
@@ -13,20 +13,31 @@ $_token=md5($ip.$time.$secret);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="web/css/page.css" rel="stylesheet">
     <style>
-        .content{display:flex;font-size:16px;}
-        .content .preview{width:200px;height:200px;border:1px solid #222;border-radius:5px;background:#ccc;}
+        .sec{padding:20px 50px !important;}
+        .sec .head{height:30px;line-height:30px;font-size: 18px;margin: 10px 0;font-family: Arial,Helvetica,sans-serif;color:#f90;}
+        /**/
+        .content{display:flex;font-size:14px;}
+        .content .preview{width:200px;height:200px;border:1px solid #222;border-radius:5px;background:#666;}
         .content .right{flex-grow:1;margin-left:20px;padding:10px 0;}
-        .content .right .btnGroup{}
-
-        .content .right .fileShow{margin:10px 0;}
+        .content .right .fileShow{margin:10px 0;line-height:26px;}
         .content .right .fileShow .fName{}
-        .content .right .fileShow .fSize{font-size:14px;color: #666;}
+        .content .right .fileShow .fSize{font-size:14px;}
         .content .right .pg-wrap{background:#ccc;height:14px;border-radius:3px;}
-        .content .right .pg-wrap .pg{background:linear-gradient(to bottom,#f90,#c60);height:14px;border-radius:3px;width:50%;}
+        .content .right .pg-wrap .pg{background:linear-gradient(to bottom,#f90,#c60);height:14px;border-radius:3px;width:0;}
         .content .right .pg-info{margin:10px 0;}
         .content .right .pg-info .per{  }
         .content .right .pg-info .speed{  }
         .content .right .pg-info .spare{ }
+        .content .right .btnGroup{}
+        .content .right .btnGroup .btn{padding: 5px 15px;border-radius: 3px;}
+        .content .right .btnGroup .btn[disabled]{background:gray;cursor:not-allowed;}
+        /**/
+        .part{margin:20px 0;line-height:30px;}
+        .part label{display:block;font-size:16px;color:#f90;}
+        .part label:after{content:":";}
+        .part input{width:100%;height:28px;border:none !important;border-radius:3px;}
+        .part select{width:300px;height:28px;border:none !important;border-radius:3px;}
+        .part .btn{padding:5px 20px !important;width:100px;border-radius:3px;}
     </style>
 </head>
 <body>
@@ -36,39 +47,48 @@ $_token=md5($ip.$time.$secret);
 
     <div class="sec">
         <div class="head">
-            Upload Video
+            上傳視頻
         </div>
         <div class="content">
             <img class="preview">
             <div class="right">
                 <div class="btnGroup">
-                    <button class="addBtn btn btn2">添加...</button>
+                    <button class="btn btn2 addBtn">添加...</button>
+                    <input id="vInput" type="file" accept="video/mp4" style="display:none;">
                 </div>
                 <div class="fileShow">
-                    <div class="fName">test.mp4</div>
-                    <div class="fSize">200M</div>
+                    <div class="fName">僅支持MP4格式，文件名不能包含除【0-9a-zA-Z_-】之外的字符。</div>
+                    <div class="fSize">文件大小在【1M,200M】之間</div>
                 </div>
                 <div class="pg-wrap">
                     <div class="pg"></div>
                 </div>
                 <div class="pg-info">
-                    <span class="per">20%</span><span>&nbsp;</span>
-                    <span class="speed">3 M/s</span>&nbsp;-&nbsp;<span class="finish">30 M</span>
-                    <span>&nbsp;剩餘時間&nbsp;</span><span class="spare">30:34</span>
+                    <span class="per">0%</span><span>&nbsp;</span>
+                    <span class="speed">0 M/s</span>&nbsp;-&nbsp;<span class="finish">0 M</span>
+                    <span>&nbsp;剩餘時間&nbsp;</span><span class="spare">00:00</span>
                 </div>
                 <div class="btnGroup">
                     <button class="startBtn btn btn2" disabled="disabled">上傳</button>
                     <button class="cancelBtn btn btn2" disabled="disabled">取消</button>
                 </div>
             </div>
-            <input id="vInput" type="file" accept="video/mp4" style="display:none;">
+        </div>
+        <div class="part">
+            <label>標題</label>
+            <input type="text" placeholder="title">
+        </div>
+        <div class="part">
+            <label>分類</label>
+            <select>
+                <option>1</option>
+                <option>2</option>
+            </select>
+        </div>
+        <div class="part">
+           <button class="btn btn2">提交</button>
         </div>
 
-        <input type="text" placeholder="title">
-        <select>
-            <option>1</option>
-            <option>2</option>
-        </select>
 
     </div>
 </div>
--- video/web/js/upload.js
+++ video/web/js/upload.js
@@ -1,32 +1,69 @@
 var upload={};
 upload.nodes={
     vShow:$(".content .preview"),
-    vInput:$(".content #vInput"),
+    vInput:function(){ return $(".content #vInput");},
     addBtn:$(".content .addBtn"),
     startBtn:$(".content .startBtn"),
     cancelBtn:$(".content .cancelBtn"),
+    fName:$(".content .fName"),
+    fSize:$(".content .fSize"),
     vPg:$(".content .pg-wrap .pg"),
     vPer:$(".content .pg-info .per"),
     vSpeed:$(".content .pg-info .speed"),
-    vSpare:$(".content .pg-info .spare")
+    vSpare:$(".content .pg-info .spare"),
+    vFinish:$(".content .pg-info .finish")
 };
 upload.task=null;
+upload.data=null;
 upload.upSpeed={st:0,sb:0};
-upload.onChange=function(){
-    var _this=this;
-    _this.nodes.startBtn.prop("disabled",false);
-    _this.nodes.cancelBtn.prop("disabled",false);
-};
 upload.init=function(){
     var _this=this;
     _this.initUpload();
-    _this.nodes.vInput.attr("onchange","upload.onChange()");
-    _this.nodes.addBtn.click(function(){_this.nodes.vInput.click();});
-    _this.nodes.cancelBtn.click(function(){_this.task && _this.task.abort();});
+    _this.nodes.addBtn.click(function(){
+        _this.nodes.vInput().click();
+    })
+    _this.nodes.startBtn.click(function(){
+        if(!_this.data) return;
+        var data=_this.data;
+        _this.nodes.addBtn.prop("disabled",true);
+        _this.nodes.startBtn.prop("disabled",true);
+        _this.nodes.cancelBtn.prop("disabled",false);
+        $.getJSON("http://lindakai.com/upload/index.php?_token="+_token+"&_time="+_time,{vInput:data.files[0].name},function(rs){
+            data.uploadedBytes = rs.vInput && rs.vInput.size;
+            if(data.uploadedBytes==data.files[0].size){
+                var name=data.files[0].name;
+                _this.nodes.startBtn.prop("disabled",true);
+                _this.nodes.cancelBtn.prop("disabled",true);
+                _this.nodes.addBtn.prop("disabled",false);
+                //
+                _this.nodes.vFinish.text(_this.formatSize(data.uploadedBytes));
+                _this.nodes.vPg.css("width","100%");
+                _this.nodes.vPer.text("100%");
+                _this.deal(name);
+            }else{
+                _this.task=data.submit();
+                _this.upSpeed.st=new Date().getTime();
+                _this.upSpeed.sb=data.uploadedBytes;
+                _this.task.success && _this.task.success(function(file){
+                    var name=file.vInput[0].name;
+                    _this.nodes.startBtn.prop("disabled",true);
+                    _this.nodes.cancelBtn.prop("disabled",true);
+                    _this.nodes.addBtn.prop("disabled",false);
+                    _this.deal(name);
+                });
+            }
+        });
+    });
+    _this.nodes.cancelBtn.click(function(){
+        _this.task && _this.task.abort();
+        _this.nodes.addBtn.prop("disabled",false);
+        _this.nodes.startBtn.prop("disabled",false);
+        _this.nodes.cancelBtn.prop("disabled",true);
+    });
 };
 upload.initUpload=function(){
     var _this=this;
-    _this.nodes.vInput.fileupload({
+    _this.nodes.vInput().fileupload({
         maxChunkSize: 2000000, // 2MB
         dataType: "json",
         paramName:"vInput",
@@ -35,40 +72,22 @@ upload.initUpload=function(){
         add: function (e, data) {
             var file=data.files[0];
             if(!/^[0-9a-zA-Z_-]+\.mp4$/.test(file.name)){
-                log("文件名不符合規則，不能包含除0-9a-zA-Z_-之外的字符");
+                _this.log("文件名不符合規則，不能包含除0-9a-zA-Z_-之外的字符");
                 return;
             }
             if(file.type!="video/mp4"){
-                log("文件類型不被接受，僅支持MP4格式");
+                _this.log("文件類型不被接受，僅支持MP4格式");
                 return;
             }
             if(file.size>200*1024*1024||file.size<1*1024*1024){
-                log("文件太大或者太小，必須在[1M,200M]之間");
+                _this.log("文件太大或者太小，必須在[1M,200M]之間");
                 return;
             }
+            //
             _this.showFile(file);
-            var that = this;
-            _this.nodes.startBtn.click(function(){
-                $.getJSON("http://lindakai.com/upload/index.php?_token="+_token+"&_time="+_time,{vInput:data.files[0].name},function(rs){
-                    data.uploadedBytes = rs.vInput && rs.vInput.size;
-                    if(data.uploadedBytes==data.files[0].size){
-                        var name=data.files[0].name;
-                        _this.nodes.startBtn.prop("disabled",true);
-                        _this.nodes.cancelBtn.prop("disabled",true);
-                        _this.deal(name);
-                    }else{
-                        _this.task=data.submit();
-                        _this.upSpeed.st=new Date().getTime();
-                        _this.upSpeed.sb=data.uploadedBytes;
-                        _this.task.success && _this.task.success(function(file){
-                            var name=file.vInput[0].name;
-                            _this.nodes.startBtn.prop("disabled",true);
-                            _this.nodes.cancelBtn.prop("disabled",true);
-                            _this.deal(name);
-                        });
-                    }
-                });
-            });
+            _this.nodes.startBtn.prop("disabled",false);
+            _this.nodes.cancelBtn.prop("disabled",true);
+            _this.data=data;
         },
         progressall:function(e,data) {
             var now=new Date().getTime();
@@ -80,17 +99,20 @@ upload.initUpload=function(){
             _this.upSpeed.sb=data.loaded;
             var speed_str=_this.formatSpeed(speed);
             var spare_str=_this.formatSpare(spare);
-
             //
-            console.log(speed_str);
-            console.log(spare_str);
+            _this.nodes.vSpeed.text(speed_str);
+            _this.nodes.vSpare.text(spare_str);
+            _this.nodes.vFinish.text(_this.formatSize(data.loaded));
             //
             var progress = Math.round(data.loaded/data.total*100);
             _this.nodes.vPg.css("width",progress+"%");
             _this.nodes.vPer.text(progress+"%");
-        }
+        },
     });
 };
+upload.log=function(msg){
+    alert(msg);
+}
 upload.formatSpeed=function(speed){
     // byte/s
     if(speed>=1024*1024){
@@ -114,7 +136,7 @@ upload.formatSize=function(size){
     }
 };
 upload.formatSpare=function(spare){
-    //s
+    if(spare==Infinity) return "00:00";
     var h,m,s;
     if(spare>60*60){
         h = Math.floor(spare/(60*60));
@@ -131,7 +153,7 @@ upload.formatSpare=function(spare){
         if(s<10){
             s="0"+s;
         }
-        return h+"時"+m+":"+s;
+        return h+":"+m+":"+s;
     }else if(spare>60){
         m = Math.floor(spare/60);
         spare-=m*60;
@@ -148,7 +170,7 @@ upload.formatSpare=function(spare){
         if(s<10){
             s="0"+s;
         }
-        return s;
+        return "00"+":"+s;
     }
 };
 upload.deal=function(name){
@@ -166,7 +188,9 @@ upload.deal=function(name){
     });
 };
 upload.showFile=function(file){
-    console.log(file);
+    var _this=this;
+    _this.nodes.fName.text(file.name);
+    _this.nodes.fSize.text(_this.formatSize(file.size));
 };
 
 $(function(){
